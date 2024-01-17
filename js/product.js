4b3111ed05a5232195e75974328086194b19a5e6
MAHMOUDSAEED <112736787+MahmoudSaeed-source@users.noreply.github.com>
2024-01-17 11:58:58 +0200

    update

--- js/product.js
+++ js/product.js
@@ -4,23 +4,27 @@ let categories_Icons_Container = document.querySelector(
 );
 let All_Products_Dom = document.querySelector(".All_Products .container .row");
 let categories_Li = [];
+let Btn_Add_To_Cart = [];
+let products_In_Cart = [];
 
 //  start get data
 (async function getData() {
   let data = await axios.get("../data/products.json");
   let res = [];
   try {
-     res = await data.data
-  } catch(err) {
+    res = await data.data;
+  } catch (err) {
     console.log(err);
   }
-  return Put_Category(res),default_Product_display(res) ;
-}())
+  return (
+    Put_Category(res), default_Product_display(res)
+  );;
+})();
 //  end get data
-// start put category in dom 
+// start put category in dom
 function Put_Category(data) {
-  for(let category in data) {
-    let first_element_Image = data[category][0].urlImage;;
+  for (let category in data) {
+    let first_element_Image = data[category][0].urlImage;
     categories_Icons_Container.innerHTML += ` <li class="category" style="background-image: url('${first_element_Image}'); "  data-category =${category}>
               <div class="layer_text"  data-category =${category}>
                 <span>${category}</span>
@@ -28,37 +32,45 @@ function Put_Category(data) {
           </li>`;
   }
   categories_Li = document.querySelectorAll(".layer_text");
-  get_All_Product_To_Dom(categories_Li,data);
+  get_All_Product_To_Dom(categories_Li, data);
 }
 // end put category in dom
-// start default category display 
+// start default category display
 function default_Product_display(data) {
   let electronic_category = data.الالكترونيات;
   electronic_category.forEach((product) => {
     putElement(product);
-  })  
+  });
 }
 default_Product_display();
-// end default category display 
-// start get All product 
+
+// end default category display
+// start get All product
 
 function get_All_Product_To_Dom(categories,data) {
+  let products = [];
+  let category_element;
   categories.forEach((category) => {
-    category.addEventListener("click",(e) => {
+    category.addEventListener("click", (e) => {
       e.stopPropagation();
       All_Products_Dom.innerHTML = "";
-     let category_element = e.target.dataset.category;
-     let products = data[category_element];
-     console.log(products);
+       category_element = e.target.dataset.category;
+       products = data[category_element];
+    });
      products.forEach((product) => {
-      putElement(product);
-     })
-   });
- });
+       putElement(product);
+     });
+  });
 }
 // put product in html
+
 function putElement(product) {
-   All_Products_Dom.innerHTML += `  <div class="product_Card">
+  All_Products_Dom.innerHTML += `  <div class="product_Card">
+ <div class="sale_icon animate__animated animate__bounceInRight">
+  <div class="slid_show "data-id= ${product.id}>
+  <i class="fa-solid fa-cart-arrow-down " data-id = ${product.id}></i>
+  </div>
+    </div>
             <div class="image_Container">
               <img src=${product.urlImage}>
             </div>
@@ -68,5 +80,26 @@ function putElement(product) {
               <span class="point">${product.point} نقطه</span>
             </div>
           </div>`;
+  Btn_Add_To_Cart = document.querySelectorAll(".slid_show");
+  Add_To_cart(product);
 }
-// end get All product 
+// end get All product
+// add to cart
+//loop فى مشكله فى اضافه العناصر لانه بيضيف اخر عنصر فقط بسب 
+// function Add_To_cart(product) {
+//   Btn_Add_To_Cart.forEach((Btn) => {
+//     Btn.addEventListener("click",() => {
+//       let Counter = document.querySelector(".count_In_Cart");
+//       let product_Add = products_In_Cart.find((pro) => pro.id === product.id)
+//       products_In_Cart.push({ ...product,quantity: 1 });
+//         Counter.innerHTML = products_In_Cart.length;
+//         console.log(products_In_Cart)
+   
+//        get_Product_Form_cart();
+//     });
+//   });
+// }
+
+// function get_Product_Form_cart() {
+ 
+// }
